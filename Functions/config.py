6965663d160fc31a530bd0582104bd6cdb9ff5eb
kulisekkk <kulisekk@proton.me>
2026-08-10 21:35:34 +0200

    Initial Commit

---
+++ Functions/config.py
@@ -0,0 +1,5 @@
+import tomllib
+
+def load_config() -> dict:
+    with open("config.toml", "rb") as f:
+        return tomllib.load(f)
---
+++ app.py
@@ -0,0 +1,17 @@
+import flask
+from flask import render_template, redirect, request, Flask
+import tomllib
+from Functions.config import load_config
+
+
+
+app = Flask(__name__, template_folder="Pages")
+
+
+@app.route("/")
+def index():
+    return load_config()
+
+
+if __name__ == "__main__": 
+    app.run(host="127.0.0.1", port=1111)
