e0dcc2b9b211c87b6844ba10821a50dc95f835b7
kulisekdev <kulisekk@proton.me>
2026-08-11 02:47:20 +0200

    added a route /system/usage for usage reporting and made it trigger the new functions in Functions/usage.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 from flask import render_template, redirect, request, Flask
 import tomllib
 from Functions.config import get_config
+from Functions.usage import get_system_usage
 
 
 app = Flask(__name__, template_folder="Pages")
@@ -10,3 +11,7 @@ app = Flask(__name__, template_folder="Pages")
 @app.route("/")
 def index():
     return "OK!", 300
+
+@app.get("/system/usage")
+def usage_report():
+    return get_system_usage()
