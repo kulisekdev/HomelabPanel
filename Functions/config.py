bda13a27b9181f153b2113699d898d22b48b463f
kulisekdev <kulisekk@proton.me>
2026-08-13 00:34:26 +0200

    Created config manipulation, not tested yet. just implementation.

--- Functions/config.py
+++ Functions/config.py
@@ -1,5 +1,42 @@
 import tomllib
+import tomli_w
+import traceback
 
-def get_config(section: str) -> dict:
-    with open("config.toml", "rb") as f:
-        return tomllib.load(f)[section]
+def get_config(file: str, section: str) -> dict:
+    try: 
+        if not isinstance(file, str):
+            raise TypeError(f"file must be str, got {type(file).__name__}")
+        if not isinstance(section, str):
+            raise TypeError(f"section must be str, got {type(section).__name__}")
+        
+        with open(file, "rb") as f:
+            return tomllib.load(f)[section]
+    except FileNotFoundError:
+        return {"msg": "File not found!", "success": False}
+    except KeyError:
+        return {"msg": f"Section '{section}' not found!", "success": False}
+    except PermissionError:
+        return {"msg": f"Permissions denied, check file permissions.", "success": False}
+    except TypeError as e:
+        return {"msg": str(e), "success": False} 
+    except Exception:
+        return {"msg": f"General Exception: {traceback.format_exc()}", "success": False}
+
+def set_config(file: str, section: str, name: str, value):
+    # get current config.
+    current_config = get_config(file)
+
+    # load new entry into the section -> name -> value
+    current_config[section][name] = value
+    try: 
+        with open("config.toml", "wb") as f:
+            tomli_w.dump(current_config, f)
+        return {"msg": "Successfully written new config.", "success": True}
+    except FileNotFoundError:
+        return {"msg": "File not found!", "success": False}
+    except KeyError as e:
+        return {"msg": f"Section '{e}' not found!", "success": False}
+    except PermissionError:
+        return {"msg": f"Permissions denied, check file permissions.", "success": False}
+    except Exception:
+        return {"msg": f"General Exception: {traceback.format_exc()}", "success": False}
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import flask
 from flask import render_template, redirect, request, Flask
 import tomllib
-from Functions.config import get_config
+from Functions.config import get_config, set_config
 from Functions.usage import get_system_usage
 
 
@@ -15,3 +15,7 @@ def index():
 @app.get("/system/usage")
 def usage_report():
     return get_system_usage()
+
+@app.get("/system/config")
+def get_configuration():
+    return get_config(2232, "app")
